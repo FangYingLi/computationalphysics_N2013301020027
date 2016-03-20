3b4184223eee2683c92226b372508b4c6ab69369
FangYingLi <963503934@qq.com>
2016-03-20 15:31:16 +0800

    p

---
+++ chapter1/parachutist.py
@@ -0,0 +1,21 @@
+from pylab import*
+v=[]
+t=[]
+a=10
+b=1
+dt=0.1
+endtime=100
+v.append(0)
+t.append(0)
+for i in range(int(endtime/dt)):
+	v.append(v[i]+(a-b*v[i])*dt)
+	t.append(t[i]+dt)
+	print t[-1] ,v[-1]
+
+plot(t,v,'go-')
+title('the velocity of a parachutist')
+xlabel('t')
+ylabel('v')
+show()
+savefig("p_.jpg")
+
