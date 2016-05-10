d9ca628762107a7abe0b0d83663e71ec20d316d1
FangYingLi <963503934@qq.com>
2016-05-10 22:15:50 +0800

    continue planet problem

--- chapter4/3pl.py
+++ chapter4/3pl.py
@@ -3,11 +3,11 @@ def g(r,p):#(radius,period)
     return vector(0,2*pi*r/p,0)#get velocity vector,(0,vy,0)
 
 #draw five stationary spheres,which their position,color,size
-mercury=sphere(pos=(0.39,0,0),color=color.blue,radius=0.06,make_trail=True)
-venus=sphere(pos=(0.72,0,0),color=color.yellow,radius=0.06,make_trail=True)
-earth=sphere(pos=(1.0,0,0),color=color.green,radius=0.06,make_trail=True)
-mars=sphere(pos=(1.52,0,0),color=color.red,radius=0.06,make_trail=True)
-sun=sphere(pos=(0,0,0),color=color.orange,radius=0.2,make_trail=True)
+mercury=sphere(pos=(0.39,0,0),color=color.blue,radius=0.06, make_trail=True)
+venus=sphere(pos=(0.72,0,0),color=color.yellow,radius=0.06, make_trail=True)
+earth=sphere(pos=(1.0,0,0),color=color.green,radius=0.06, make_trail=True)
+mars=sphere(pos=(1.52,0,0),color=color.red,radius=0.06, make_trail=True)
+sun=sphere(pos=(0,0,0),color=color.orange,radius=0.2, make_trail=True)
 
 mercury.v=g(0.39,0.241)#v means velocity
 venus.v=g(0.72,0.615)
--- chapter4/twobody.py
+++ chapter4/twobody.py
@@ -1,15 +1,15 @@
 from math import *
 from pylab import *
-x1,y1,x2,y2=[-1],[0],[1],[0]
-vx1,vy1,vx2,vy2=[0],[-pi],[0],[pi]
-dt=0.002
+x1,y1,x2,y2=[-1.],[0.],[1.],[0.]
+vx1,vy1,vx2,vy2=[0.],[-pi],[0.],[pi]
+dt=0.001
 t=[0]
 while t[-1]<10:
     r=sqrt((x1[-1]-x2[-1])**2+(y1[-1]-y2[-1])**2)
     vx1.append(vx1[-1]+dt*(-4*pi*pi*(x1[-1]-x2[-1])/(r**3)))
     vx2.append(vx2[-1]+dt*(-4*pi*pi*(x2[-1]-x1[-1])/(r**3)))
     vy1.append(vy1[-1]+dt*(-4*pi*pi*(y1[-1]-y2[-1])/(r**3)))
-    vy2.append(vx2[-1]+dt*(-4*pi*pi*(y2[-1]-y1[-1])/(r**3)))
+    vy2.append(vy2[-1]+dt*(-4*pi*pi*(y2[-1]-y1[-1])/(r**3)))
     x1.append(x1[-1]+vx1[-1]*dt)
     x2.append(x2[-1]+vx2[-1]*dt)
     y1.append(y1[-1]+vy1[-1]*dt)
@@ -20,6 +20,8 @@ plot(x1,y1,'r')
 plot(x2,y2,'b')
 xlabel('x')
 ylabel('y')
+xlim(-0.1,0.1)
+ylim(-1.05,-0.98)
 title('two stars with same mass')
 show()
 show()
