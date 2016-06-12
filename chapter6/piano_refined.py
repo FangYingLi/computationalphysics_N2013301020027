8483f3da4dab180cbf959e9881f3dcb3a8f517be
HE Chong <chong.he.1989@gmail.com>
2016-06-12 15:34:06 +0800

    update for animation

--- chapter6/piano_refined.py
+++ chapter6/piano_refined.py
@@ -8,7 +8,7 @@ from matplotlib import animation
 from pylab import*
 import numpy as np
 
-epsilon = 0.
+epsilon = 1e-8
 c_const=300.0
 dx=0.01
 dt = dx/c_const
@@ -25,11 +25,23 @@ def iterator(stop_time, t_interval, init_cond, update_rule):
     """
     result = [init_cond, init_cond]
     time = 0
+    initial_energy = string_energy_per_cell(result[-2:], t_interval)
     while time < stop_time:
         result.append(update_rule(result[-2:], t_interval))
+#        result[-1]=np.array(result[-1])*(initial_energy/string_energy_per_cell(
+#            result[-2:], t_interval))**0.5
         time+=t_interval
     del result[0] # remove the redundant initial condition
     return result
+
+def string_energy_per_cell(array_curr, t_interval):
+    array_now = array_curr[-1]
+    array_prev = array_curr[-2]
+    return np.average([
+        ((right-left)/(2*dx))**2*c_const**2 + ((center-up)/t_interval)**2
+        for center, left, right, up in
+        zip(array_now[1:-1], array_now[0:-2], array_now[2:], array_prev[1:-1])
+        ])
 
 def update_rule(array_curr, t_interval):
     """ periodic boundary condition
@@ -38,17 +50,23 @@ def update_rule(array_curr, t_interval):
     r = c_const * t_interval/dx
     M = length/dx
     # prepare array for iteration
-    array_now = \
-        [-array_curr[-1][1]]+list(array_curr[-1])+[-array_curr[-1][-2]]
-    # padding the current array
+    array_now = array_curr[-1]
     array_prev = array_curr[-2]
+    # stiff-free
     borderless = [
-        (2.-2.*r**2.-6.*epsilon*r**2.*M**2.)*center - up +
-        r**2.*(1.+4.*epsilon*M**2.)*(right_1+left_1) -
-        epsilon*r**2.*M**2.*(right_2+left_2)
-        for center, left_1, left_2, right_1, right_2, up in
-        zip(array_now[2:-2], array_now[1:-3], array_now[0:-4], array_now[3:-1],
-            array_now[4:], array_prev[1:-1])]
+        (2.-2.*r**2)*center - up + r**2*(right_1+left_1)
+        for center, left_1, right_1, up in
+        zip(array_now[1:-1], array_now[0:-2], array_now[2:], array_prev[1:-1])
+        ]
+    # padding stiff effect
+    borderless[1:-1] = [
+        stiff_free -6.*epsilon*r**2.*M**2.*center
+        +4.*epsilon*M**2.*(right_1+left_1)
+        -epsilon*r**2.*M**2.*(right_2+left_2)
+        for stiff_free, center, right_1, left_1, right_2, left_2 in
+        zip(borderless[1:-1], array_now[2:-2], array_now[1:-3],
+            array_now[0:-4], array_now[3:-1], array_now[4:])
+        ]
     return [0.] + borderless + [0.]     # padding the boundary condtion.
 
 def fix_boundary_rule(array_curr, t_interval):
@@ -64,15 +82,22 @@ def fix_boundary_rule(array_curr, t_interval):
 init_array = 0.001*np.exp(-1000.*(string-0.55)**2)
 init_array[0] = init_array[-1] = 0.
 
-a=iterator(stop_time=0.5, t_interval=dt, init_cond=init_array,
+a=iterator(stop_time=0.50, t_interval=dt, init_cond=init_array,
            update_rule=update_rule)
 a01=iterator(stop_time=0.05, t_interval=dt, init_cond=init_array,
              update_rule=fix_boundary_rule)
-#print cmp(a,a01)
-'''
-for iter in a01:
-   print type(a01)
-   '''
+
+f=figure()
+ax=axes(xlim=(0,1),ylim=(-0.0020,0.0020))
+line, =ax.plot(string,init_array,lw=2)
+
+def animate(i):
+    line.set_ydata(a[i])
+    return line,
+anim=animation.FuncAnimation(f,animate,frames=range(len(a)),blit=True)#frames mean zhenshu,interval mean each frame last how long
+show()
+
+
 b=[a_iter[5] for a_iter in a]
 b01=[a01_iter[5] for a01_iter in a01]
 e=len(b)
@@ -80,14 +105,14 @@ e01=len(b01)
 d=linspace(0,0.01/300*e,e)
 d01=linspace(0,0.01/300*e01,e01)
 #print d
-'''
+
 plot(d,b)
 xlim(0,0.05)
 xlabel('Time(s)')
 ylabel('Signal(arbitary units)')
 title('String signal versus time')
 show()
-'''
+
 y2=np.fft.fft(b)
 f=np.fft.fftfreq(len(b),0.01/300)
 y2=abs(y2)**2
@@ -102,5 +127,3 @@ f01=np.fft.fftfreq(len(b01),0.01/300)
 y21=abs(y21)**2
 plot(f01,y21,':',color='r')
 xlim(0,3000)
-
-
